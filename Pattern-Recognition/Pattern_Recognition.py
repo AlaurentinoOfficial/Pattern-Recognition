ca349e0e7a6880dafe78616cfd02a5dff6c2fd54
Anderson Laurentino <alaurentino.br@gmail.com>
2017-01-02 05:11:37 -0200

    Update Pattern_Recognition.py

--- Pattern-Recognition/Pattern_Recognition.py
+++ Pattern-Recognition/Pattern_Recognition.py
@@ -1,49 +1,25 @@
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import numpy as np
 
-from sklearn import datasets
 from sklearn import svm
+from skimage import color
+from skimage import io
 
-def data_to_image(sample):
-    matriz = []
-    array = []
-    for x in xrange(0, 64):
-        array.append(sample[x])
-        if (x+1)%8 == 0:
-            matriz.append(array)
-            array = []
-    return np.array(matriz)
+svc = svm.SVC(gamma=0.05, cache_size=100)
 
-# Take digit pictures
-digits = datasets.load_digits()
+pen = color.rgb2gray(io.imread('images/pen.jpg'))[...,2];
+pen_2 = color.rgb2gray(io.imread('images/pen2.jpg'))[...,2];
+pen_3 = color.rgb2gray(io.imread('images/pen3.jpg'))[...,2];
+car = color.rgb2gray(io.imread('images/car.jpg'))[...,2];
+car_2 = color.rgb2gray(io.imread('images/car2.png'))[...,2];
+notebook = color.rgb2gray(io.imread('images/notebook.png'))[...,2];
 
-# Classifer
-svc = svm.SVC(gamma=0.05, C=100)
+input, target = [pen, car, notebook], ['pen', 'car', 'notebook']
+svc.fit(input, target)
 
-x_data = np.array([16,0,0,0,0,0,0,16,
-                      0,16,0,0,0,0,16,0,
-                      0,0,16,0,0,16,0,0,
-                      0,0,0,16,16,0,0,0,
-                      0,0,0,16,16,0,0,0,
-                      0,0,16,0,0,16,0,0,
-                      0,16,0,0,0,0,16,0,
-                      16,0,0,0,0,0,0,16])
-
-o_data = np.array([16,16,16,16,16,16,16,16,
-                     16,0,0,0,0,0,0,16,
-                     16,0,0,0,0,0,0,16,
-                     16,0,0,0,0,0,0,16,
-                     16,0,0,0,0,0,0,16,
-                     16,0,0,0,0,0,0,16,
-                     16,0,0,0,0,0,0,16,
-                     16,16,16,16,16,16,16,16])
-
-# Training
-x, y = [x_data, o_data], ['x', 'o']
-svc.fit(x,y)
-        
-
-# Prediction
-print('Prediction: ', svc.predict(x_data)[0])
-plt.imshow(data_to_image(x_data), cmap=plt.cm.gray_r, interpolation="nearest")
-plt.show()
+print 'Prediction: %s' % svc.predict(pen)[0]
+print 'Prediction: %s' % svc.predict(pen_2)[0]
+print 'Prediction: %s' % svc.predict(pen_3)[0]
+print 'Prediction: %s' % svc.predict(car)[0]
+print 'Prediction: %s' % svc.predict(car_2)[0]
+print 'Prediction: %s' % svc.predict(notebook)[0]
